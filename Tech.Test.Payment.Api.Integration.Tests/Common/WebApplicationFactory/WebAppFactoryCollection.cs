702573448d8eaa93d3e12639c02da5494b660d8f
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2024-11-03 22:20:11 -0300

    removendo código não utilizando

--- Tech.Test.Payment.Api.Integration.Tests/Common/WebApplicationFactory/WebAppFactoryCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tech.Test.Payment.Api.Integration.Tests.Common.WebApplicationFactory;
-
-[CollectionDefinition(CollectionName)]
-public class WebAppFactoryCollection : ICollectionFixture<WebAppFactory>
-{
-    public const string CollectionName = "WebAppFactoryCollection";
-}
